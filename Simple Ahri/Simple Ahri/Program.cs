39b5f0af9c8795b7d245f504309e53bdd94860e0
FireBuddy <aijun@live.ca>
2016-06-21 13:29:13 -0400

    Update Program.cs

--- Simple Ahri/Simple Ahri/Program.cs
+++ Simple Ahri/Simple Ahri/Program.cs
@@ -142,11 +142,11 @@ namespace SimpleAhri
             {
                return;
             }
-            if (!sender.IsDashing() && sender.Type == GameObjectType.AIHeroClient && sender.IsValidTarget(900) && Spellmanager.Q.IsReady() && sender.IsEnemy)
+            if (!sender.IsDashing() && sender.Type == GameObjectType.AIHeroClient && sender.IsValidTarget(900) && SpellManager.Q.IsReady() && sender.IsEnemy)
             {
                 
                 {
-                    Spellmanager.Q.Cast(sender.ServerPosition);
+                    SpellManager.Q.Cast(sender.ServerPosition);
                 }
 
             } 
