e25802fb034b639f83ecc245451ff4e70de6161b
FireBuddy <aijun@live.ca>
2016-06-23 12:31:54 -0400

    Update Program.cs

--- Simple Ahri/Simple Ahri/Program.cs
+++ Simple Ahri/Simple Ahri/Program.cs
@@ -139,7 +139,7 @@ namespace SimpleAhri
 
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            if (sender == null && (!Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass) ||(!Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit)))
+            if (sender == null && (!Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass) || !Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit)))
             {
                return;
             }
