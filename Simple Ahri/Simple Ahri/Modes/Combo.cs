315d6c78f8a78471ebb00841dd4e2c771e81332d
FireBuddy <aijun@live.ca>
2016-06-29 19:14:50 -0400

    Update Combo.cs

--- Simple Ahri/Simple Ahri/Modes/Combo.cs
+++ Simple Ahri/Simple Ahri/Modes/Combo.cs
@@ -52,7 +52,7 @@ namespace SimpleAhri.Modes
                 {
                     E.Cast(prediction.CastPosition);
                 }
-                if (prediction.HitChancePercent >= 80 && (target.IsInRange(Player.Instance, 800) && NavMesh.IsWallOfGrass(Player.Instance.Position, 0))
+                if (prediction.HitChancePercent >= 80 && (target.IsInRange(Player.Instance, 800) && NavMesh.IsWallOfGrass(Player.Instance.Position, 5))
                 {
                     E.Cast(prediction.CastPosition);
                 }
