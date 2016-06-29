3995f8e8caf1caa444e15744ceb91761766655d7
FireBuddy <aijun@live.ca>
2016-06-29 18:33:00 -0400

    Update Program.cs

--- Simple Ahri/Simple Ahri/Program.cs
+++ Simple Ahri/Simple Ahri/Program.cs
@@ -96,7 +96,7 @@ namespace SimpleAhri
         
         private static void Game_OnGameUpdate(EventArgs args)
         {
-           if (GetKeyBind( Combo(), "Combo EFlash"))
+           if (Settings.UseRAfterPlayer)
             {
               
             }
