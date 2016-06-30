50fe267b9ae2776aa3d9defa5325a41065d5488f
FireBuddy <aijun@live.ca>
2016-06-30 12:50:34 -0400

    Update Flee.cs

--- Simple Ahri/Simple Ahri/Modes/Flee.cs
+++ Simple Ahri/Simple Ahri/Modes/Flee.cs
@@ -12,15 +12,19 @@ namespace SimpleAhri.Modes
 
         public override void Execute()
         {
+            {
             var target = Program.CurrentTarget;
             if (Q.IsReady() && Config.Modes.Flee.UseQ && target.IsInRange(Player.Instance, Q.Range + 300))
             {
                 Q.Cast(target);
             }
+            }
+            {
             
             if (Q.IsReady() && Config.Modes.Flee.UseQ)
             {
                 Q.Cast(Player.Instance.Position.Extend(Game.CursorPos, -300).To3D());
+            }
             }
         }
     }
