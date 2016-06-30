b067d534c30477c58d40ec0c37930400189f3ccf
FireBuddy <aijun@live.ca>
2016-06-29 20:01:32 -0400

    Update Combo.cs

--- Simple Ahri/Simple Ahri/Modes/Combo.cs
+++ Simple Ahri/Simple Ahri/Modes/Combo.cs
@@ -31,7 +31,7 @@ namespace SimpleAhri.Modes
                 }
             }
 
-            if (Settings.UseQ && Q.IsReady() && target.IsInRange(Player.Instance, Q.Range))
+            if (Settings.UseQ && Q.IsReady() && target.IsInRange(Player.Instance, Q.Range) && !target.HasSpellShield())
             {
                 var prediction = Q.GetPrediction(target);
                 if (prediction.HitChancePercent >= 70)
