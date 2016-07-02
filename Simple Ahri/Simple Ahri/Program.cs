8037c7e44aba7351386f486d7f5f791aa6c63183
FireBuddy <aijun@live.ca>
2016-07-02 14:40:15 -0400

    Update Program.cs

--- Simple Ahri/Simple Ahri/Program.cs
+++ Simple Ahri/Simple Ahri/Program.cs
@@ -218,7 +218,7 @@ namespace SimpleAhri
                     SpellManager.Q.Cast(sender.ServerPosition);
 
                 }                
-                if (args.Target != null))
+                if (args.Target != null)
                 {
                     Chat.Print("targetspell");
                     SpellManager.Q.Cast(sender.ServerPosition);
